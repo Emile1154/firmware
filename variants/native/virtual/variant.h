cd1fa25cc4c0ac21d14102bb3ef33bdf72f462c3
Emile1154 <laim788@gmail.com>
2026-01-17 16:24:04 +0500

    add native_virtual target build - pio run -e native_virtual

---
+++ variants/native/virtual/variant.h
@@ -0,0 +1,12 @@
+// #ifndef HAS_SCREEN
+// #define HAS_SCREEN 0
+// #endif
+// #define USE_TFTDISPLAY 0
+#define CANNED_MESSAGE_MODULE_ENABLE 1
+#define HAS_ETHERNET 1
+// #define HAS_GPS 1
+#define MAX_RX_TOPHONE portduino_config.maxtophone
+#define MAX_NUM_NODES portduino_config.MaxNodes
+
+// RAK12002 RTC Module
+// #define RV3028_RTC (uint8_t)0b1010010
