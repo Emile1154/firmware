a04b893b5b785e24f43659a961f2615ff649baad
e.bagmanov <e.bagmanov@k-soft-spb.ru>
2026-01-27 08:23:56 +0000

    [upd] [Power.cpp] restored call deInitApiServer function for linux target

--- src/Power.cpp
+++ src/Power.cpp
@@ -22,7 +22,8 @@
 #include "sleep.h"
 
 #if defined(ARCH_PORTDUINO)
-#include "api/WiFiServerAPI.h"
+#include "api/TCPServer.h"
+// #include "api/WiFiServerAPI.h"
 #include "input/LinuxInputImpl.h"
 #endif
 
@@ -749,7 +750,7 @@ void Power::reboot()
 #elif defined(ARCH_RP2040)
     rp2040.reboot();
 #elif defined(ARCH_PORTDUINO)
-    // deInitApiServer();
+    deInitApiServer();
     if (aLinuxInputImpl)
         aLinuxInputImpl->deInit();
     SPI.end();
