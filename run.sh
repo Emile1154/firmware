af0bc90eed6d26f21e1580fb5ba33878424059a2
e.bagmanov <e.bagmanov@k-soft-spb.ru>
2026-01-30 13:52:29 +0000

    [add] run.sh script to launch virtual node as software in the loop simulation component

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Check for the required number of arguments
+if [ "$#" -ne 4 ]; then
+    echo "Usage: <local_port> <remote_port> <webserver_port> <mac_addr>"
+    exit 1
+fi
+MAC_ADDR="${4//:/}"  # This removes all colons
+
+
+if [ ${#MAC_ADDR} -ne 12 ]; then
+    echo "Error: MAC address must be 12 hex digits after formatting."
+    exit 1
+fi
+
+# Define the path to the executable
+MESHTASTICD="$(pwd)/.pio/build/native_virtual/meshtasticd"
+
+"$MESHTASTICD" -s -v -l "$1" -r "$2" -w "$3" -h "$MAC_ADDR"
